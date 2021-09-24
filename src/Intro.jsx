f5be7573f2975c05f33c2b450f73bdd46fadd8c2
Navjyot Jaiswal <stypertypes@gmail.com>
2021-09-25 03:55:04 +0530

    Add intro section with UI skeleton image.

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import PhoneView from './images/phoneView.png';
 
 function Intro(){
     return(
         <div className = "introContainer">
-            
+            <img src = {PhoneView} width = "450" height= "880"/>
         </div>
     );
 }

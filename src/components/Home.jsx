7ecc54c0eb7ce48c0d7a1684a58e2a87f434e369
Navjyot Jaiswal <stypertypes@gmail.com>
2021-09-26 11:11:02 +0530

    Add 'how it works' section.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Hero from './Hero';
+import Process from './Process';
 
 function Home(){
     return(
         <div>
             <Navbar />
             <Hero />
+            <Process />
         </div>
     );
 }

c14165e58cce2f540d1aa3f811f5b96511765391
Navjyot Jaiswal <stypertypes@gmail.com>
2021-09-29 04:59:59 +0530

    remove unecssary indents

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Create from './components/Create';
@@ -12,7 +11,7 @@ function App() {
   
   return (
       <div>
-      <Navbar />
+        <Navbar />
         <Switch>
           <Route path = "/" component = {Home} exact/>
           <Route path = "/create" component = {Create} exact/>

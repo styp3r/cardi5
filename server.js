b7c6fd8da12eed0ef0b2b5ec679737e1195043c0
Navjyot Jaiswal <stypertypes@gmail.com>
2021-09-29 00:22:31 +0530

    get url input and check if the data exists in the firebase database and display snapshot object in array format if it exists.

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 import express from 'express';
 
+import database from './src/firebase.js';
+import { ref, onValue } from "firebase/database";
+
 const app = express();
 
 app.get("/", (req, res)=>{
     res.send("hello from the server!");
+});
+
+app.get("/:id", (req, res)=>{
+    const db = database;
+    const starCountRef = ref(db, 'users/'+req.params.id);
+    onValue(starCountRef, (snapshot) => {
+        if(snapshot.exists()){
+            const data = snapshot.val();
+            const entries = Object.entries(data);
+            res.send("<h1>"+entries+"</h1>");
+        }else{
+            res.send("ERROR 404");
+        }
+});   
 });
 
 app.listen(4000, ()=>{

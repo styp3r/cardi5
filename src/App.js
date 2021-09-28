a8543403fde2a1dd1e93e4170fe525e2442c6bc6
Navjyot Jaiswal <stypertypes@gmail.com>
2021-09-29 03:55:08 +0530

    Create routes using react-router-dom and point the routes to different pages - home, create, help & search.

--- src/App.js
+++ src/App.js
@@ -1,48 +1,25 @@
 import './App.css';
-import {useState} from 'react';
-import database from './firebase';
-import { ref, set } from "firebase/database";
+import { Switch, Route } from 'react-router-dom';
 
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Create from './components/Create';
+import Help from './components/Help';
+import Search from './components/Search';
   
 function App() {
-  const [name , setName] = useState();
-  const [age , setAge] = useState();
-  //const [show, updateShow] = useState();
-      
-  // Push Function
-  function writeUserData() {
-    const db = database;
-    set(ref(db, 'users/'+name), {
-      username: name,
-      age: age,
-    });
-  }
-
-  /* Pull Function
-  function readUserData(){
-    const db = database;
-    const starCountRef = ref(db, 'users/');
-    onValue(starCountRef, (snapshot) => {
-    const data = snapshot.val();
-    updateShow(data.username + " " + data.age);
-
-    <button onClick = {readUserData}>READ</button>
-      <p>{show}</p>
-});
-} */
+  
   
   return (
-    <div className="App" style={{marginTop : 250}}>
-      <center>
-      <input placeholder="Enter your name" value={name} 
-      onChange={(e) => setName(e.target.value)}/>
-      <br/><br/>
-      <input placeholder="Enter your age" value={age} 
-      onChange={(e) => setAge(e.target.value)}/>
-      <br/><br/> 
-      <button onClick={writeUserData}>PUSH</button>
-      </center>
-    </div>
+      <div>
+      <Navbar />
+        <Switch>
+          <Route path = "/" component = {Home} exact/>
+          <Route path = "/create" component = {Create} exact/>
+          <Route path = "/help" component = {Help} exact/>
+          <Route path = "/search" component = {Search} exact/>
+        </Switch>
+      </div>
   );
 }
   

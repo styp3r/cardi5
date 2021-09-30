774b42f97394569cfc0818be85bc498b33f46c00
Navjyot Jaiswal <stypertypes@gmail.com>
2021-09-30 13:31:21 +0530

    Add styling to link buttons and html body background color.

--- server.js
+++ server.js
@@ -12,9 +12,10 @@ app.get("/:id", (req, res)=>{
         if(snapshot.exists()){
             const data = snapshot.val();
             const entries = Object.entries(data);
-            console.log(data.username);
-            for(let i = 0; i < (entries.length-1); i++){
-                res.write("<h4 style = 'width: 30vmin; border-radius: 2vmin ; background-color: #4d94ff; padding: 2vmin;'><a style = 'text-decoration: none; color: #ffffff;' href = "+entries[i][1]+">Facebook</a></h4>");
+            res.write("<body style = 'width: 100%; height: 100%; overflow: hidden; background-image: linear-gradient("+data.zcolor+", #ffffff);'></body>");
+            res.write("<h3 style = 'display: inline-block; margin-left: 50%; transform: translateX(-50%); margin-top: 10vmax;'>"+data.username+"</h3>");
+            for(let i = 0; i < (entries.length-2); i++){
+                res.write("<h4 style = 'text-align: center; margin-left: 50%; transform: translateX(-50%); margin-top: 2vmax; width: 30vmin; border-radius: 2vmin ; background-color: #4d94ff; padding: 2vmin;'><a style = 'text-decoration: none; color: #ffffff;' href = "+entries[i][1]+"/>Facebook</h4>");
             }
             res.send();
         }else{

bf7362e2b9e07105af3c15f1f6eec43fcdb9789f
Navjyot Jaiswal <stypertypes@gmail.com>
2021-10-01 13:33:31 +0530

    Redesign input fields and buttons.

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -22,12 +22,12 @@ function Create(){
 
     return(
         <div id = "newElementId">
-            <h1>Create</h1>
-            <h1>{count}</h1>
-            <button id = "addNewLink" onClick = {()=>setCount(count + 1)}>add link</button>
-            <button id = "removeLink" onClick = {()=>setCount(count - 1)}>delete link</button>
+            <h1 className = "dashboardHeader">Get Started</h1>
+            <div className = "creatorDashboard">
+            <button id = "addNewLink" onClick = {()=>setCount(count + 1)}><i class="fas fa-plus"></i> Add New Link</button>
+            <button id = "removeLink" onClick = {()=>setCount(count - 1)}><i class="fas fa-minus"></i></button>
             <GenerateLinks count = {count}/>
-            
+            </div>
         </div>
     );
 }
